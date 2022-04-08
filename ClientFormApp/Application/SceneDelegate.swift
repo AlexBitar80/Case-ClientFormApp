5a44afe57e7c492c274bde2548e054dc566ab2a6
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-08 16:12:19 -0300

    added main screen preview

--- ClientFormApp/Application/SceneDelegate.swift
+++ ClientFormApp/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
                 
         let safeWindow = UIWindow(windowScene: windowScene)
-        safeWindow.rootViewController = UINavigationController(rootViewController: ClientInfoViewController())
+        safeWindow.rootViewController = UINavigationController(rootViewController: ViewController())
         safeWindow.frame = UIScreen.main.bounds
         safeWindow.makeKeyAndVisible()
         
