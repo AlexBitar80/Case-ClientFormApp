212165bad0530cb42dc70e156b866086daa109bf
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-08 16:14:38 -0300

    added client address fields

--- ClientFormApp/Controllers/ViewController.swift
+++ ClientFormApp/Controllers/ViewController.swift
@@ -9,33 +9,29 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    private lazy var containerScrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.backgroundColor = .white
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        return scrollView
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
-    private lazy var contentView: UIStackView = {
+    private lazy var contentStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 32
-        stackView.backgroundColor = .blue
-        stackView.distribution = .fillEqually
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
 
     private lazy var zipCodeTextField: ZipCodeTextField = {
         let textField = ZipCodeTextField(placeholder: "Cep")
-        textField.backgroundColor = .systemGray5
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
 
     private lazy var streetTextField: AddressTextField = {
         let textField = AddressTextField(placeholder: "Logradouro")
-        textField.backgroundColor = .systemGray5
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
@@ -82,46 +78,60 @@ final class ViewController: UIViewController {
 
     private lazy var addNewClientButton: UIButton = {
         let button = UIButton()
+        button.layer.cornerRadius = 8.0
+        button.setTitle("Adicionar Cliente", for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .purple
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .yellow
-        
-        self.title = "Lista de clientes"
+        self.title = "Informações de endereço"
         
         setup()
     }
     
     private func setup() {
-        view.addSubview(containerScrollView)
-        containerScrollView.addSubview(contentView)
-        contentView.addArrangedSubview(zipCodeTextField)
-        contentView.addArrangedSubview(streetTextField)
-        contentView.addArrangedSubview(numberTextField)
-        contentView.addArrangedSubview(districtTextField)
-        contentView.addArrangedSubview(additionalAddressTextField)
-        contentView.addArrangedSubview(cityTextField)
-        contentView.addArrangedSubview(stateTextField)
-        contentView.addArrangedSubview(addNewClientButton)
+        view.addSubview(containerView)
+        containerView.addSubview(contentStackView)
+        contentStackView.addArrangedSubview(zipCodeTextField)
+        contentStackView.addArrangedSubview(streetTextField)
+        contentStackView.addArrangedSubview(numberTextField)
+        contentStackView.addArrangedSubview(districtTextField)
+        contentStackView.addArrangedSubview(additionalAddressTextField)
+        contentStackView.addArrangedSubview(cityTextField)
+        contentStackView.addArrangedSubview(stateTextField)
+        containerView.addSubview(addNewClientButton)
         
         NSLayoutConstraint.activate([
-            containerScrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            containerScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            containerScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            containerScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            containerView.topAnchor.constraint(equalTo: view.topAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         
         NSLayoutConstraint.activate([
-            contentView.topAnchor.constraint(equalTo: containerScrollView.topAnchor),
-            contentView.trailingAnchor.constraint(equalTo: containerScrollView.trailingAnchor),
-            contentView.leadingAnchor.constraint(equalTo: containerScrollView.leadingAnchor),
-            contentView.bottomAnchor.constraint(equalTo: containerScrollView.bottomAnchor),
-            
-            contentView.widthAnchor.constraint(equalTo: containerScrollView.widthAnchor),
+            contentStackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 80),
+            contentStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            contentStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+        ])
+        
+        NSLayoutConstraint.activate([
+            numberTextField.heightAnchor.constraint(equalToConstant: 40),
+            districtTextField.heightAnchor.constraint(equalToConstant: 40),
+            numberTextField.heightAnchor.constraint(equalToConstant: 40),
+            additionalAddressTextField.heightAnchor.constraint(equalToConstant: 40),
+            cityTextField.heightAnchor.constraint(equalToConstant: 40),
+            stateTextField.heightAnchor.constraint(equalToConstant: 40),
+        ])
+        
+        NSLayoutConstraint.activate([
+            addNewClientButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            addNewClientButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+            addNewClientButton.heightAnchor.constraint(equalToConstant: 50),
+            addNewClientButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -16),
         ])
     }
 }
