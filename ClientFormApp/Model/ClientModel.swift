41bfef60fbe74c46d3e061c05a1a7d92d331e360
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-08 16:13:52 -0300

    added client data model

---
+++ ClientFormApp/Model/ClientModel.swift
@@ -0,0 +1,20 @@
+//
+//  ClientModel.swift
+//  ClientFormApp
+//
+//  Created by João Alexandre on 08/04/22.
+//
+
+import Foundation
+
+struct ClientModel {
+    let name: String?
+    let email: String?
+    let phone: String?
+    let zipCode: String?
+    let number: String?
+    let street: String?
+    let address: String?
+    let city: String?
+    let state: String?
+}
