21f16457f706715eac875d86255d6b18cc0165a6
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-09 15:15:13 -0300

    separating data models

--- ClientFormApp/Model/ClientModel.swift
+++ ClientFormApp/Model/ClientModel.swift
@@ -7,10 +7,23 @@
 
 import Foundation
 
-struct ClientModel {
+struct ClientModel: Codable {
+    var info: InfoModel?
+    var address: AddressModel?
+    
+    init(info: InfoModel, address: AddressModel) {
+        self.info = info
+        self.address = address
+    }
+}
+
+struct InfoModel: Codable {
     let name: String?
     let email: String?
     let phone: String?
+}
+
+struct AddressModel: Codable {
     let zipCode: String?
     let number: String?
     let street: String?
